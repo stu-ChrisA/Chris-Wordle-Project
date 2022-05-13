fe2cbeb88ac56961c406e398c71bc046a6f84c4f
stu-ChrisA <92584525+stu-ChrisA@users.noreply.github.com>
2022-05-13 13:04:14 -0400

    Update Wordle.java
    Added milestone #1 and 2

--- Wordle.java
+++ Wordle.java
@@ -9,7 +9,9 @@ import edu.willamette.cs1.wordle.WordleDictionary;
 import edu.willamette.cs1.wordle.WordleGWindow;
 
 public class Wordle {
-
+    private int randomIndex = (int) (Math.random() * WordleDictionary.FIVE_LETTER_WORDS.length);
+    private String answerToWordle = WordleDictionary.FIVE_LETTER_WORDS[randomIndex].toUpperCase();
+    
     public void run() {
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
@@ -21,7 +23,23 @@ public class Wordle {
  */
 
     public void enterAction(String s) {
-        gw.showMessage("You have to implement this method.");
+        String userWord = s;
+        userWord = userWord.toLowerCase();
+        boolean check = false;
+        for(String checkWord : WordleDictionary.FIVE_LETTER_WORDS)
+        {
+            if(userWord.equals(checkWord))
+            {
+                check = true;
+            }
+        }
+        if(!check)
+        {
+            gw.showMessage("not in word list.");
+
+        }else{
+            gw.showMessage("in word list.");
+        }
     }
 
 /* Startup code */
@@ -29,6 +47,16 @@ public class Wordle {
     public static void main(String[] args) {
         new Wordle().run();
     }
+    public void RandomWord()
+    {
+        int letter = 0;
+        for(int c = 0; c < WordleGWindow.N_COLS; c++)
+        {
+            gw.setSquareLetter(0,c,answerToWordle.substring(letter,letter+1));
+            letter++;
+        }
+    }
+    
 
 /* Private instance variables */
 
